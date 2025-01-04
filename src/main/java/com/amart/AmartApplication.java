2636182c4242c65191d269b3e80635f02366ac99
Akshay Andhale <andhaleakshay6775@gmail.com>
2025-01-04 12:17:35 +0530

    initial commit

---
+++ src/main/java/com/amart/AmartApplication.java
@@ -0,0 +1,13 @@
+package com.amart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amart/AmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
